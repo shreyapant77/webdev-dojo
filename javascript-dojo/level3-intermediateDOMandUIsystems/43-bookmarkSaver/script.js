c17cc77bf3aa31cf92748b46b383aa800b19efe0
SHREYA PANT <shreyapant77@gmail.com>
2026-08-02 17:44:57 +0530

    project 43 complete with visit and delete btn

--- javascript-dojo/level3-intermediateDOMandUIsystems/43-bookmarkSaver/script.js
+++ javascript-dojo/level3-intermediateDOMandUIsystems/43-bookmarkSaver/script.js
@@ -13,9 +13,26 @@ function addBookmark(){
     let box=document.createElement("div");
     let p1=document.createElement("p");
     let p2=document.createElement("p");
+
+    let visitBtn=document.createElement("button");
+
+    visitBtn.addEventListener("click",function(){
+        window.open(bookmark.url,"_blank");
+    });
+
+    let delBtn=document.createElement("button");
+    delBtn.innerText="delete bookmark"
+
+    delBtn.addEventListener("click",function(){
+        box.remove();
+    })
+
     p1.innerText=bookmark.name;
     p2.innerText=bookmark.url;
+    visitBtn.innerText="visit now"
     box.appendChild(p1);
     box.appendChild(p2);
+    box.appendChild(visitBtn);
+    box.appendChild(delBtn);
     containerAccess.appendChild(box);
 }
